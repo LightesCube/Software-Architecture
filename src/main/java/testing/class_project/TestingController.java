092309bfc67c25a35526af094cae51c5ef3f6426
Doggie Husky <jamarquez145@gmail.com>
2025-05-19 09:26:02 -0400

    añadido cross origin

--- src/main/java/testing/class_project/TestingController.java
+++ src/main/java/testing/class_project/TestingController.java
@@ -9,11 +9,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST Controller for employee database operations.
@@ -22,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class TestingController {
 
